0f8a337c029c79a202f6c0e10d8d297914c28b25
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-14 16:39:39 +0100

    Added footer with links to github and netlify

--- src/App.js
+++ src/App.js
@@ -8,6 +8,34 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="logo" className="Designer-logo img-fluid" />
         <h1>Dictionary Application</h1>
+        <footer>
+          <div className="social-links">
+            Coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/sophie-davies-52910b72"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sophie Davies
+            </a>
+            , opensourced on{" "}
+            <a
+              href="https://github.com/SophieLMD/react-dictionary-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://react-dictionary-app-slmd.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify.
+            </a>
+          </div>
+        </footer>
       </header>
     </div>
   );
