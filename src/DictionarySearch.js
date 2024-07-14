5b9cce4ad6d17a27025ffbf5c168241f2060a016
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-07-14 17:11:58 +0100

    Added centering to form

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,4 +1,16 @@
 import React from "react";
+import "./DictionarySearch.css";
 export default function DictionarySearch() {
-  return <div>Hello World</div>;
+  return (
+    <div className="Dictionary">
+      <form>
+        <input
+          type="search"
+          placeholder="Search for a word..."
+          autoFocus={true}
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
